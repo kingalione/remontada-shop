db11f4686ba86eeb43d8b5fa121c60fe34f98175
Ali Karadag <akrdg1988@icloud.com>
2019-12-12 20:14:12 +0100

    Creat page for sign in and sign up

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './sign-in-and-sign-up.styles.sass';
+
+const SignInAndSignUp = () => (
+  <div className="sign-in-and-sign-up">SIGN IN</div>
+);
+
+export default SignInAndSignUp;
