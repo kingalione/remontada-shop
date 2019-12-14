82593b52d17ebd098ae8f4e4e2f5280d7cd24cc7
Ali Karadag <akrdg1988@icloud.com>
2019-12-14 22:26:10 +0100

    Add Sign in with google feature

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
+import { signInWithGoogle } from '../../firebase/firebase.utils';
+
 import './sign-in.styles.sass';
 
 class SignIn extends React.Component {
@@ -49,6 +51,9 @@ class SignIn extends React.Component {
             required
           />
           <CustomButton type="submit"> SIGN IN </CustomButton>
+          <CustomButton onClick={signInWithGoogle}>
+            Sign in with Google
+          </CustomButton>
         </form>
       </div>
     );
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DB_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  apiKey: 'AIzaSyAx3AGsyMBCulV1_0-KSRJgfnSIXyXSwLw',
+  authDomain: 'remontada-db.REACT_APPapp.com',
+  databaseURL: 'https://remontada-db.REACT_APPio.com',
+  projectId: 'remontada-db',
+  storageBucket: 'remontada-db.appspot.com',
+  messagingSenderId: '1023941467506',
+  appId: '1:1023941467506:web:898d5a683a6251159611eb',
+  measurementId: 'G-GL64ZW7LGC'
 };
 
 firebase.initializeApp(config);
