14109f96fdc11fcfb7b838189288237084b51924
Ali Karadag <akrdg1988@icloud.com>
2019-12-14 22:12:53 +0100

    Remove dotenv

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
-require('dotenv').config();
-
 function App() {
   return (
     <div>
