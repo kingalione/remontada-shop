789dfcc4f8226b8fbb0cace7a9af9534c79ee5bc
Ali Karadag <akrdg1988@icloud.com>
2019-12-15 01:58:03 +0100

    Pass new created or existing user to state

--- src/App.js
+++ src/App.js
@@ -20,9 +20,18 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      await createUserProfileDocument(user);
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: { id: snapShot.id, ...snapShot.data() }
+          });
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
